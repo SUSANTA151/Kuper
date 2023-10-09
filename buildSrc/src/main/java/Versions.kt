3fda0b10eb0a7b5fbc0a5fe491986606f128cb75
SLJ-DESIGNS <sljdesigns.skr@gmail.com>
2023-10-09 16:35:06 +0530

    Initialize project

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,19 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "8.1.1"
+    const val kotlin = "1.9.10"
+    const val ksp = "$kotlin-1.0.13"
+
+    // OneSignal
+    const val oneSignal = "4.8.6"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 34
+    const val buildTools = "34.0.0"
+
+    // Kuper
+    const val kuper = "2.4.9"
+}
